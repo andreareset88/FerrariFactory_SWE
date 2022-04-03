37fe9aed9927293f193f4ff7ad25a4fb1be53249
andreareset88 <61541785+andreareset88@users.noreply.github.com>
2022-04-03 19:07:57 +0200

    Aggiunta classe Catalogo

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		int numeroLaFerrari = (int)(Math.random() * 6);
 		int numeroSf90Stradale = (int)(Math.random() * 6);
@@ -15,11 +15,21 @@ public class main {
 		int metodo = scanner.nextInt();
 		riv.setMethod(metodo);
 		acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
+		Thread.sleep(5000);
 		acq.setTipoAuto(0);
 		acq.setVersione(0);
+		System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
+		Scanner scanner2 = new Scanner(System.in);
+		int metodo2 = scanner2.nextInt();
+		riv.setMethod(metodo2);
 		acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
+		Thread.sleep(5000);
 		acq.setTipoAuto(2);
 		acq.setVersione(1);
+		System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
+		Scanner scanner3 = new Scanner(System.in);
+		int metodo3 = scanner3.nextInt();
+		riv.setMethod(metodo3);
 		acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 	}
 }
