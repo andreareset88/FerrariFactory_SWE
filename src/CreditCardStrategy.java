90dfd994d8f28cb7610728a063ff3d9ac4634a4a
andreareset88 <61541785+andreareset88@users.noreply.github.com>
2022-04-17 16:50:48 +0200

    Se inserito cvv sbagliato più di 3 volte, termina esecuzione

--- src/CreditCardStrategy.java
+++ src/CreditCardStrategy.java
@@ -9,19 +9,29 @@ public class CreditCardStrategy implements PaymentStrategy {
 	
 	@Override
 	public void pay(float amount) throws InterruptedException {
-		System.out.println("Inserire cvv per la verifica");
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		int code = scanner.nextInt();
-		if(acq.getCvv() == code) {
-			System.out.println("Carta "+acq.getCardNumber()+" accettata, tentativo di pagamento in corso...");
-			Thread.sleep(3000);
-			float budget = acq.getBudget();
-			budget -= amount;
-			System.out.println("Pagamento di "+amount+" €, effettuato dal Sig. "+acq.getNome()+" riuscito, il budget rimanente è di "+budget+" €");
-			acq.setBudget(budget);
+		int tentativiRimanenti = 3;
+		boolean correct = false;
+		while(!correct) {
+			System.out.println("Inserire cvv per la verifica ("+tentativiRimanenti+" tentativi rimanenti)");
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			int code = scanner.nextInt();
+			if (acq.getCvv() == code) {
+				correct = true;
+				System.out.println("Carta " + acq.getCardNumber() + " accettata, tentativo di pagamento in corso...");
+				Thread.sleep(3000);
+				float budget = acq.getBudget();
+				budget -= amount;
+				System.out.println("Pagamento di " + amount + " €, effettuato dal Sig. " + acq.getNome() + " riuscito, il budget rimanente è di " + budget + " €");
+				acq.setBudget(budget);
+			} else {
+				System.out.println("Il codice cvv non è corretto");
+				tentativiRimanenti--;
+				if(tentativiRimanenti == 0){
+					System.out.println("Attenzione, tentativi esauriti!");
+					System.exit(0);
+				}
+			}
 		}
-		else
-			System.out.println("Il codice cvv non è corretto"); // TODO migliora controllo cvv
 	}
 }
