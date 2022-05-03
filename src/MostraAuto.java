378088e94c0540b1d0e7f08688ffc7452477a110
andreareset88 <61541785+andreareset88@users.noreply.github.com>
2022-05-03 17:39:56 +0200

    La macchina viene scelta solo se è disponibile nel catalogo

--- src/MostraAuto.java
+++ src/MostraAuto.java
@@ -21,9 +21,9 @@ public final class MostraAuto implements Observer{
     }
 
     public void draw(int tipoAuto) throws IOException {
-        File laferrari = new File("C:\\Users\\acer\\OneDrive\\Immagini\\ferrari-laferrari.jpg");
-        File sf90 = new File("C:\\Users\\acer\\OneDrive\\Immagini\\sf90stradale.jpg");
-        File testarossa = new File("C:\\Users\\acer\\OneDrive\\Immagini\\Ferrari-Testarossa.jpg");
+        File laferrari = new File("C:\\Users\\acer\\OneDrive\\Immagini\\la-ferrari.jpg");
+        File sf90 = new File("C:\\Users\\acer\\OneDrive\\Immagini\\sf90-stradale.jpg");
+        File testarossa = new File("C:\\Users\\acer\\OneDrive\\Immagini\\testarossa.jpg");
         BufferedImage image;
         if(tipoAuto == 0)
             image = ImageIO.read(laferrari);
@@ -33,7 +33,7 @@ public final class MostraAuto implements Observer{
         ImageIcon imageIcon = new ImageIcon(image);
         JFrame jFrame = new JFrame();
         jFrame.setLayout(new FlowLayout());
-        jFrame.setSize(2048, 1536);
+        jFrame.setSize(824, 494);
         JLabel jLabel = new JLabel();
         jLabel.setIcon(imageIcon);
         jFrame.add(jLabel);
--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ public class main {
 
 		Catalogo cat = new Catalogo();
 		// Acquirente inizializzato con SF90 Stradale deluxe
-		Acquirente acq = Acquirente.getInstance(1, 1, 7500000, "Charles", 789543, 997, "MN654GH");
+		Acquirente acq = Acquirente.getInstance(1, 1, 10000000, "Charles", 789543, 997, "MN654GH");
 		Rivenditore riv = Rivenditore.getInstance(acq, null, cat);
 		MostraAuto gestore = new MostraAuto(riv);
 		gestore.setRiv();
@@ -15,13 +15,13 @@ public class main {
 		ControlloBudget.setAcq(acq);
 		riv.pubblicizza();
 		if(acq.getIndiceGradimento() > 50) {
-			System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
-			Scanner scanner = new Scanner(System.in);
-			int metodo = scanner.nextInt();
-			riv.setMethod(metodo);
 			// Qui entrano in gioco Abstract Factory e Observer
-			acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 			if(cat.getNumeroSf90Stradale() > 0) {
+				System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
+				Scanner scanner = new Scanner(System.in);
+				int metodo = scanner.nextInt();
+				riv.setMethod(metodo);
+				acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 				cat.setNumeroSf90Stradale(cat.getNumeroSf90Stradale() - 1);
 				float polizza = acq.calcolaPolizza(1050);
 				if (!acq.cancellaOrdine(polizza)) {
@@ -42,14 +42,15 @@ public class main {
 		riv.pubblicizza();
 		if(acq.getIndiceGradimento() >= 50) {
 			// LaFerrari standard
-			acq.setTipoAuto(0);
-			acq.setVersione(0);
-			System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
-			Scanner scanner2 = new Scanner(System.in);
-			int metodo2 = scanner2.nextInt();
-			riv.setMethod(metodo2);
-			acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
+			//acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 			if(cat.getNumeroLaFerrari() > 0) {
+				acq.setTipoAuto(0);
+				acq.setVersione(0);
+				System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
+				Scanner scanner2 = new Scanner(System.in);
+				int metodo2 = scanner2.nextInt();
+				riv.setMethod(metodo2);
+				acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 				cat.setNumeroLaFerrari(cat.getNumeroLaFerrari() - 1);
 				float polizza = acq.calcolaPolizza(900);
 				if(!acq.cancellaOrdine(polizza)) {
@@ -70,14 +71,15 @@ public class main {
 		riv.pubblicizza();
 		if(acq.getIndiceGradimento() >= 50) {
 			// Testarossa deluxe
-			acq.setTipoAuto(2);
-			acq.setVersione(1);
-			System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
-			Scanner scanner3 = new Scanner(System.in);
-			int metodo3 = scanner3.nextInt();
-			riv.setMethod(metodo3);
-			acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
+			//acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 			if(cat.getNumeroTestarossa() > 0) {
+				acq.setTipoAuto(2);
+				acq.setVersione(1);
+				System.out.println("Scegliere metodo di pagamento (0 per carta di credito, 1 per bonifico)");
+				Scanner scanner3 = new Scanner(System.in);
+				int metodo3 = scanner3.nextInt();
+				riv.setMethod(metodo3);
+				acq.scegliMacchina(acq.getTipoAuto(), acq.getVersione());
 				cat.setNumeroTestarossa(cat.getNumeroTestarossa() - 1);
 				float polizza = acq.calcolaPolizza(470);
 				if(!acq.cancellaOrdine(polizza)) {
@@ -97,6 +99,7 @@ public class main {
 		acq.detach(riv);
 		riv.detach(gestore);
 		riv.mostraResoconto(autoVendute);
+		System.exit(0);
 	}
 }
 
